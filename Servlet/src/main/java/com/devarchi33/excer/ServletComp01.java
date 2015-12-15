eaa9233fd8f2f7548f059d2dbf3ae56775d2e35e
skyfly33 <imfly7@naver.com>
2015-12-15 14:24:16 +0900

    Add Servlet example module.

---
+++ Servlet/src/main/java/com/devarchi33/excer/ServletComp01.java
@@ -0,0 +1,56 @@
+package com.devarchi33.excer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+/**
+ * Created by donghoon on 2015. 12. 15..
+ */
+@WebServlet(name = "com.devarchi33.excer.ServletComp01", urlPatterns = {"/test"})
+public class ServletComp01 extends HttpServlet {
+    // Method to handle GET method request.
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        // Set response content type
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String title = "HTTP Header Request Example";
+        String docType =
+                "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                        "transitional//en\">\n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<table width=\"100%\" border=\"1\" align=\"center\">\n" +
+                "<tr bgcolor=\"#949494\">\n" +
+                "<th>Header Name</th><th>Header Value(s)</th>\n" +
+                "</tr>\n");
+
+        Enumeration headerNames = request.getHeaderNames();
+
+        while (headerNames.hasMoreElements()) {
+            String paramName = (String) headerNames.nextElement();
+            out.print("<tr><td>" + paramName + "</td>\n");
+            String paramValue = request.getHeader(paramName);
+            out.println("<td> " + paramValue + "</td></tr>\n");
+        }
+        out.println("</table>\n</body></html>");
+    }
+
+    // Method to handle POST method request.
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
---
+++ Servlet/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<p><a href="/test">Servlet Test</a></p>
+</body>
+</html>
