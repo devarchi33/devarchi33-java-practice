aa2b582efa85150cdbf4728ebeb781835a98ea67
skyfly33 <imfly7@naver.com>
2015-12-30 01:51:19 +0900

    에코서버에 옵션 적용.

--- Netty/src/main/java/com/devarchi33/excer/server/EchoServer.java
+++ Netty/src/main/java/com/devarchi33/excer/server/EchoServer.java
@@ -1,10 +1,7 @@
 package com.devarchi33.excer.server;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -24,6 +21,7 @@ public class EchoServer {
             ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1)
                     .handler(new LoggingHandler(LogLevel.INFO))
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
